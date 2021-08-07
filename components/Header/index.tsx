c7b802dca96b2a3509186d377be8e5d527ad5810
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-08-07 19:49:26 +0900

    unused props

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,9 +3,7 @@ import { Logo } from './Logo';
 import { Menu } from './Menu';
 import { SearchBar } from './SearchBar';
 
-interface Props {}
-
-const index = (props: Props) => {
+const index = () => {
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       <Logo />
