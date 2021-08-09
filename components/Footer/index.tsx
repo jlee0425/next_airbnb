b267fb7828ac74fe3b7ba0ad54ff13b4da92d3f1
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-08-10 01:40:06 +0900

    changed element name for accessibility

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,11 +5,11 @@ import { FooterDiv } from './FooterDiv';
 const index = () => {
   return (
     // gap is better than space-y, but gap may not be supported on Safari
-    <div className='grid gird-cols-1 md:grid-cols-4 md:space-y-0 sm:space-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+    <footer className='grid gird-cols-1 md:grid-cols-4 md:space-y-0 sm:space-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
       {footerInfo.map(({ title, tags }: FooterInfoProps) => (
         <FooterDiv title={title} tags={tags} key={`footer_${title}`} />
       ))}
-    </div>
+    </footer>
   );
 };
 
