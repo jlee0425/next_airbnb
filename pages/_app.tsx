4a2f98db80d0f16948502bb49bd6aa83dd1517e9
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-08-10 01:39:37 +0900

    progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import 'tailwindcss/tailwind.css';
 import 'swiper/swiper.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
+import 'styles/global.css';
+import ProgressBar from '@badrap/bar-of-progress';
+import { Router } from 'next/router';
+
+const progressBar = new ProgressBar({
+  size: 4,
+  color: '#FD5B61',
+  className: 'z-50',
+  delay: 100
+});
+
+Router.events.on('routeChangeStart', progressBar.start);
+Router.events.on('routeChangeComplete', progressBar.finish);
+Router.events.on('routeChangeError', progressBar.finish);
 
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />;
